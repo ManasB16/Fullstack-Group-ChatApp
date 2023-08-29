9af72c850f3800450b288af65625989a82e6e384
Manas Bhola <manasbhola@gmail.com>
2023-08-29 11:54:25 +0530

    Optimizing Chat App

--- Controllers/chat.js
+++ Controllers/chat.js
@@ -1,5 +1,6 @@
 const Chat = require("../Models/Chat");
 const sequelize = require("../Util/database");
+const { Op } = require("sequelize");
 
 const postChat = async (req, res, next) => {
   try {
@@ -18,7 +19,14 @@ const postChat = async (req, res, next) => {
 
 const getChat = async (req, res, next) => {
   try {
-    const allChats = await Chat.findAll();
+    const chatId = req.query.lastchatid;
+    const allChats = await Chat.findAll({
+      where: {
+        id: {
+          [Op.gt]: chatId,
+        },
+      },
+    });
     res.status(200).json({ allChats });
   } catch (err) {
     res.status(500).json({
--- Views/chat.js
+++ Views/chat.js
@@ -1,5 +1,30 @@
 const token = localStorage.getItem("token");
 
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    refresh();
+  } catch (err) {
+    document.body.innerHTML += `<div style="color: red;text-align: center;">
+                                    <h3>${err}</h3>
+                              </div>`;
+  }
+});
+
+function parseJwt(token) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map((c) => {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
+
 async function onSend(e) {
   try {
     e.preventDefault();
@@ -18,26 +43,65 @@ async function onSend(e) {
     alert("Chat Sent");
     document.getElementById("msg").value = "";
   } catch (err) {
-    document.body.innerHTML += `<p>${err}</p>`;
+    document.body.innerHTML += `<div style="color: red;text-align: center;">
+                                    <h3>${err}</h3>
+                              </div>`;
   }
 }
 
-window.addEventListener("DOMContentLoaded", async () => {
-  refresh();
-});
-
 async function refresh() {
-  document.getElementById("chats").innerHTML = "";
-  const getChats = await axios.get("http://localhost:4000/chat/getchat");
-  getChats.data.allChats.forEach((chat) => {
-    showNewChatOnUi(chat);
-  });
+  try {
+    document.getElementById("chats").innerHTML = "";
+    let concatedArr;
+
+    let chat = JSON.parse(localStorage.getItem("Allchats"));
+    if (chat == null || chat.length == 0 || chat == undefined) lastchatid = 0;
+    else lastchatid = chat[chat.length - 1].id;
+
+    const getChats = await axios.get(
+      `http://localhost:4000/chat/getchat?lastchatid=${lastchatid}`,
+      { headers: { Authorization: token } }
+    );
+    const backendArr = getChats.data.allChats;
+
+    if (chat == null || chat == undefined || chat.length == 0) {
+      concatedArr = [...backendArr];
+    } else {
+      concatedArr = chat.concat(backendArr);
+    }
+    if (concatedArr.length > 10) {
+      concatedArr = concatedArr.slice(concatedArr.length - 10);
+    }
+
+    const localstorageChat = JSON.stringify(concatedArr);
+    localStorage.setItem("Allchats", localstorageChat);
+
+    concatedArr.forEach((msg) => {
+      showNewChatOnUi(msg);
+    });
+  } catch (err) {
+    document.body.innerHTML += `<div style="color: red;text-align: center;">
+                                    <h3>${err}</h3>
+                              </div>`;
+  }
 }
 
 async function showNewChatOnUi(chat) {
-  const parentNode = document.getElementById("chats");
-  const childElement = `<li>${chat.name}-${chat.message}</li>`;
-  parentNode.innerHTML += childElement;
+  try {
+    let currUser = parseJwt(token);
+    const parentNode = document.getElementById("chats");
+    if (chat.userId == currUser.userId) {
+      const childElement = `<p>You: ${chat.message}</p>`;
+      parentNode.innerHTML += childElement;
+    } else {
+      const childElement = `<p>${chat.name}: ${chat.message}</p>`;
+      parentNode.innerHTML += childElement;
+    }
+  } catch (err) {
+    document.body.innerHTML += `<div style="color: red;text-align: center;">
+                                    <h3>${err}</h3>
+                              </div>`;
+  }
 }
 
 // setInterval(refresh, 1000);
